064dee0480abc724b7a25897ee2484f461b4142a
nijiyamaharjan <nijiyamn@gmail.com>
2024-10-12 19:07:29 +0545

    sort dATES

--- frontend/mood-tracker/src/pages/Home.jsx
+++ frontend/mood-tracker/src/pages/Home.jsx
@@ -1,42 +1,52 @@
-import { useEffect, useState } from 'react'
-import { useMoodsContext } from '../hooks/useMoodsContext'
-
-import MoodDetails from '../components/MoodDetails'
-import MoodForm from '../components/MoodForm'
-
-import * as React from 'react';
+import { useEffect } from 'react';
+import { useMoodsContext } from '../hooks/useMoodsContext';
+import MoodDetails from '../components/MoodDetails';
 import Box from '@mui/joy/Box';
-import Card from '@mui/joy/Card';
-import CardContent from '@mui/joy/CardContent';
 import Typography from '@mui/joy/Typography';
 
-
 export default function Home() {
-    const { moods, dispatch } = useMoodsContext()
+    const { moods, dispatch } = useMoodsContext();
 
     useEffect(() => {
         const fetchMoods = async () => {
-            const response = await fetch('http://localhost:4000/api/moods')
-            const json = await response.json()
+            const response = await fetch('http://localhost:4000/api/moods');
+            const json = await response.json();
 
             if (response.ok) {
-                dispatch({type: 'SET_MOODS', payload: json})
+                dispatch({ type: 'SET_MOODS', payload: json });
             }
-        }
+        };
 
-        fetchMoods()
-    }, [dispatch])
+        fetchMoods();
+    }, [dispatch]);
 
+    // Function to group moods by date
+    const groupMoodsByDate = (moods) => {
+        return moods.reduce((acc, mood) => {
+            const moodDate = new Date(mood.date).toLocaleDateString();
+            if (!acc[moodDate]) {
+                acc[moodDate] = [];
+            }
+            acc[moodDate].push(mood);
+            return acc;
+        }, {});
+    };
+
+    const groupedMoods = groupMoodsByDate(moods);
+
+    const sortedDates = Object.keys(groupedMoods).sort((a, b) => new Date(b) - new Date(a))
     return (
-        <div>
-
-
-            <div>
-                {moods && moods.map(mood => (
-                    <MoodDetails mood={mood} key={mood._id} />
-                ))}
-            </div>
-            
-        </div>
-    )
-  }
+        <Box sx={{ padding: 2 }}>
+            {sortedDates.map((date) => (
+                <Box key={date} sx={{ marginBottom: 4 }}>
+                    <Typography variant="h6" sx={{ borderBottom: '1px solid lightgray', pb: 1 }}>
+                        {date}
+                    </Typography>
+                    {groupedMoods[date].map((mood) => (
+                        <MoodDetails mood={mood} key={mood._id} />
+                    ))}
+                </Box>
+            ))}
+        </Box>
+    );
+}
