99525fd6f18415de1f6c66f32f9a2a2a7d791a45
nijiyamaharjan <nijiyamn@gmail.com>
2024-10-13 23:04:14 +0545

    minor changes

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,6 @@ app.use('/api/moods', moodRoutes)
 
 // connect to db
 mongoose.connect(process.env.MONGO_URI)
-// mongoose.connect('mongodb://localhost:27017/moodTracker')
     .then(() => {
         app.listen(process.env.PORT, () => {
             console.log('connected to db and listening on port 4000')
